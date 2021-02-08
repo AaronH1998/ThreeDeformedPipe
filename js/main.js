ab195fe57f52b0848b35f13dc3496b5722be61c3
Aaron Hodgson <aaronhodgson1998@gmail.com>
2021-02-08 12:42:17 +0000

    moving camera through pipe

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ const renderer = new THREE.WebGLRenderer();
 const controls = new OrbitControls( camera, renderer.domElement );
 
 //controls.update() must be called after any manual changes to the camera's transform
-camera.position.set( 5, 0, 5 );
+camera.position.set( -3, 0, 0 );
 controls.update();
 
 renderer.setSize(window.innerWidth,window.innerHeight);
@@ -22,32 +22,48 @@ document.body.appendChild(renderer.domElement);
     const color = 0xFFFFFF;
     const intensity = 1;
     const light = new THREE.AmbientLight(color,intensity);
-    // light.position.set(-1,2,4);
     scene.add(light);
 }
 
-
-
+let modelCamera;
+let initialCameraPosition;
 const loader = new GLTFLoader();
 loader.load('./models/deformedCylinderWithCamera.glb',function(gltf){
     let pipe = gltf.scene.children.filter(p=>p.name=="Cylinder")[0];
-    pipe.material = new THREE.MeshBasicMaterial( { color: 0x000000 } );
+    pipe.material = new THREE.MeshBasicMaterial({ color: 0x000000 });
     pipe.material.wireframe = true;
 
-    let modelCamera = gltf.scene.children.filter(p=>p.name=="ModelCamera")[0];
-    modelCamera.material = new THREE.MeshBasicMaterial( { color: 0xFF0000 } );
-    // gltf.scene.traverse(function(child){
-    //     if(child.isMesh){
-    //         child.material = new THREE.MeshBasicMaterial( { color: 0x000000 } );
-    //         child.material.wireframe = true;
-    //     }
-    // });
+    modelCamera = gltf.scene.children.filter(p=>p.name=="ModelCamera")[0];
+    modelCamera.material = new THREE.MeshBasicMaterial({ color: 0xFF0000 });
+    initialCameraPosition = JSON.parse(JSON.stringify(modelCamera.position));
+
     scene.add(gltf.scene);
 
     requestAnimationFrame(render);
 },undefined,function(error){
     console.log(error);
 });
+
+
+
+document.addEventListener("keydown", onDocumentKeyDown, false);
+
+function onDocumentKeyDown(event) {
+    var zSpeed = 0.02;
+    var keyCode = event.which;
+    if (keyCode == 37) {
+        event.preventDefault();
+        modelCamera.position.z -= zSpeed;
+    } else if (keyCode == 39) {
+        event.preventDefault();
+        modelCamera.position.z += zSpeed;
+    } else if (keyCode == 32) {
+        event.preventDefault();
+        modelCamera.position.set(initialCameraPosition.x,initialCameraPosition.y,initialCameraPosition.z);
+    }
+}
+
+
 
 function render(time) {
     
