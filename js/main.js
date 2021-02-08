2d088d09a39c261009b284b07e173101ab38777b
Aaron Hodgson <aaronhodgson1998@gmail.com>
2021-02-08 12:14:14 +0000

    camera model now showing

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ const renderer = new THREE.WebGLRenderer();
 const controls = new OrbitControls( camera, renderer.domElement );
 
 //controls.update() must be called after any manual changes to the camera's transform
-camera.position.set( 0, 0, 5 );
+camera.position.set( 5, 0, 5 );
 controls.update();
 
 renderer.setSize(window.innerWidth,window.innerHeight);
@@ -27,33 +27,27 @@ document.body.appendChild(renderer.domElement);
 }
 
 
-let cylinder;
+
 const loader = new GLTFLoader();
-loader.load('./models/deformedCylinder.glb',function(gltf){
-    cylinder = gltf.scene.children.filter(p=>p.name=="Cylinder")[0];
-    gltf.scene.traverse(function(child){
-        if(child.isMesh){
-            child.material = new THREE.MeshBasicMaterial( { color: 0x000000 } );
-            child.material.wireframe = true;
-        }
-    });
+loader.load('./models/deformedCylinderWithCamera.glb',function(gltf){
+    let pipe = gltf.scene.children.filter(p=>p.name=="Cylinder")[0];
+    pipe.material = new THREE.MeshBasicMaterial( { color: 0x000000 } );
+    pipe.material.wireframe = true;
+
+    let modelCamera = gltf.scene.children.filter(p=>p.name=="ModelCamera")[0];
+    modelCamera.material = new THREE.MeshBasicMaterial( { color: 0xFF0000 } );
+    // gltf.scene.traverse(function(child){
+    //     if(child.isMesh){
+    //         child.material = new THREE.MeshBasicMaterial( { color: 0x000000 } );
+    //         child.material.wireframe = true;
+    //     }
+    // });
     scene.add(gltf.scene);
 
     requestAnimationFrame(render);
 },undefined,function(error){
     console.log(error);
 });
-
-// const geometry = new THREE.BoxGeometry();
-
-// const wireframe = new THREE.WireframeGeometry(geometry);
-
-// const line = new THREE.LineSegments(wireframe);
-// line.material.depthTest = false;
-// line.material.opacity = 0.25;
-// line.material.transparent = true;
-
-// scene.add(line);
 
 function render(time) {
     
